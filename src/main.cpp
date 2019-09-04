3f30994dd6308ef19942b625a831aa7c6e494367
Yuki Koyama <yuki@koyama.xyz>
2019-09-04 18:20:51 +0900

    Add a custom widget

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <QApplication>
+#include <QWidget>
+
+class CustomWidget : public QWidget
+{
+
+};
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    CustomWidget widget;
+    widget.show();
 
     return app.exec();
 }
