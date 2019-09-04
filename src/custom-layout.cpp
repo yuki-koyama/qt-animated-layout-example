891c0fe42bb4894b00dda4934801803b8925ee6a
Yuki Koyama <yuki@koyama.xyz>
2019-09-04 18:43:06 +0900

    Add minimum item manipulation using std::vector

--- src/custom-layout.cpp
+++ src/custom-layout.cpp
@@ -1,1 +1,15 @@
 #include "custom-layout.hpp"
+
+QLayoutItem* CustomLayout::itemAt(int index) const
+{
+    return (index < 0 || index >= this->count()) ? nullptr : m_items[index];
+}
+
+QLayoutItem* CustomLayout::takeAt(int index)
+{
+    QLayoutItem* item = m_items[index];
+
+    m_items.erase(m_items.begin() + index);
+
+    return item;
+}
--- src/custom-layout.hpp
+++ src/custom-layout.hpp
@@ -2,16 +2,20 @@
 #define custom_layout_hpp
 
 #include <QLayout>
+#include <vector>
 
 class CustomLayout : public QLayout
 {
 public:
-    void         addItem(QLayoutItem* item) override {}
-    int          count() const override { return 0; }
-    QLayoutItem* itemAt(int index) const override { return nullptr; }
-    QLayoutItem* takeAt(int index) override { return nullptr; }
+    void         addItem(QLayoutItem* item) override { m_items.push_back(item); }
+    int          count() const override { return m_items.size(); }
+    QLayoutItem* itemAt(int index) const override;
+    QLayoutItem* takeAt(int index) override;
 
     QSize sizeHint() const override { return QSize(); }
+
+private:
+    std::vector<QLayoutItem*> m_items;
 };
 
 #endif /* custom_layout_hpp */
